740112a1123b63930e818c245ca0d672be77b5ca
ViceEye <ckh985169151@qq.com>
2022-07-07 23:56:12 +0100

    feat(GM): New base gamemode

---
+++ Source/BoatRace/Cpp/Cpp_BoatGM_Base.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Cpp_BoatGM_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOATRACE_API ACpp_BoatGM_Base : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+
+public:
+	
+};
